408cfad2917403346800b80972360382d7da07a6
murad <muradakhdev@gmail.com>
2017-11-03 01:10:10 -0400

    added subclasses of Safety

---
+++ app/src/main/java/tech/edt/MapApp/feature/EmergencyPhone.java
@@ -0,0 +1,11 @@
+package tech.edt.MapApp.feature;
+
+/**
+ * Created by Murad on 11/3/17.
+ */
+
+public class EmergencyPhone extends Safety {
+    EmergencyPhone(double lat, double lng, String name) {
+        super(lat, lng, name, false);
+    }
+}
