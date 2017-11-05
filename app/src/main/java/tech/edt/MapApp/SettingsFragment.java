9ce4043f227707419b6321c8f1449d269c03d0e3
ClassicPork <classicpork@gmail.com>
2017-11-04 22:18:05 -0400

    Preferences

--- app/src/main/java/tech/edt/MapApp/SettingsFragment.java
+++ app/src/main/java/tech/edt/MapApp/SettingsFragment.java
@@ -15,14 +15,12 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends PreferenceFragmentCompat{
-
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState,
                                     String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
-
     }
 
 }
