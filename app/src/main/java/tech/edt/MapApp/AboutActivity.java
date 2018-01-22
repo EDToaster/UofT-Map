c22849e563d25764a6d5cfa454205e907fd56c0c
ClassicPork <classicpork@gmail.com>
2018-01-22 18:16:13 -0500

    Uses Version number in Build Gradle file.

--- app/src/main/java/tech/edt/MapApp/AboutActivity.java
+++ app/src/main/java/tech/edt/MapApp/AboutActivity.java
@@ -1,37 +1,39 @@
 package tech.edt.MapApp;
 
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 import mehdi.sakout.aboutpage.AboutPage;
 import mehdi.sakout.aboutpage.Element;
 
 /**
  * Created by murad on 1/11/18.
- *
+ * <p>
  * A simple about window
  */
 
-public class AboutActivity extends AppCompatActivity{
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //back button
         super.onCreate(savedInstanceState);
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         //creates the about page
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
-                .setDescription("Unofficial University of Toronto Map" +
-                        " app. Designed and Developed by Howard C" +
-                        "hen and Murad Akhundov.\n\n2017-2018")
+                .setDescription("Unofficial University of Toronto map app. Designed and " +
+                        "Developed by Howard Chen and Murad Akhundov in 2018")
                 .setImage(R.drawable.uoft)
-                .addItem(new Element().setTitle("Version 0.1"))
+                .addItem(new Element().setTitle("Version " + BuildConfig.VERSION_NAME))
                 .addGitHub("https://github.com/EDToaster/MapAppRepo", "Github")
                 .create();
 
